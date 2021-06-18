3d9a4aed6371a9dec56afee17c14ed0d7ce9970d
Geniy <gytraskovskiy@sberbank.ru>
2021-06-18 18:39:49 +0300

    Link modal components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import {useSelect} from '@statirjs/react';
 import Tiptap from './components/Tiptap';
+import LinkModal from './components/LinkModal';
 
 function App() {
     const isOpen = useSelect(root => root.linkModalForm.isOpen);
-    console.log(isOpen);
     return (
         <div className="App">
             <Tiptap />
+            {isOpen ? <LinkModal /> : null}
         </div>
     );
 }
---
+++ src/components/LinkModal/LinkModal.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './LinkModal.module.scss';
+
+const LinkModal = () => {
+    console.log(styles);
+    return <div>link modal</div>
+};
+
+export default LinkModal;
